d2743654edcad2223ad29663680aac59cc265a6d
R3nt0n <r3nt0n@protonmail.com>
2018-10-06 21:48:28 +0200

    Reactstrap

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import App from '../../App'
+import './index.css'
+
+import { Alert, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { InputGroup, InputGroupAddon, InputGroupText } from 'reactstrap';
+import { FormFeedback } from 'reactstrap';
+
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isLogged: false,
+      error: false,
       user: '',
       pswd: ''
     };
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.checkAuth = this.checkAuth.bind(this);
+    this.printErrorMsg = this.printErrorMsg.bind(this);
   }
 
   checkAuth() {
@@ -39,41 +47,69 @@ class Login extends Component {
       this.props.toggleLoginBoard();
       }
     else {
-      // Falta mostrar mensaje de error
+      // Mostrar mensaje de error
+      this.setState({error: true});
+      //return(this.printErrorMsg());
     }
     event.preventDefault();
+  }
+
+  printErrorMsg() {
+    if (this.state.error) {
+      return (
+        <Alert color="danger">
+          Usuario/contraseña incorrecto
+        </Alert>
+      )
+    }
   }
 
   render() {
     return (
       <div className="App">
         <div className="container">
-          <form onSubmit={this.handleSubmit}>
-
-            <p><label>
-              Username:
-              <input
-                name="user"
-                type="text"
-                value={this.state.user}
-                onChange={this.handleInputChange} />
-            </label></p>
-
-            <p><label>
-              Password:
-              <input
-                name="pswd"
-                type="password"
-                value={this.state.pswd}
-                onChange={this.handleInputChange} />
-            </label></p>
-
-            <p><input type="submit" value="Log IN" /></p>
           
-          </form>
+          <Form onSubmit={this.handleSubmit} id="loginForm">
 
+
+            <FormText>Enter username:</FormText>
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">@</InputGroupAddon>
+              <Input
+                type="text"
+                name="user"
+                id="user"
+                value={this.state.user}
+                onChange={this.handleInputChange}
+              />
+            </InputGroup>
+            <FormFeedback valid>Sweet! that name is available</FormFeedback>
+            <FormFeedback valid tooltip>Sweet! that name is available</FormFeedback>
+            <FormFeedback>Oh noes! that name is already taken</FormFeedback>
+
+            <FormText>Enter password:</FormText>
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">#</InputGroupAddon>
+              <Input
+                  type="password"
+                  name="pswd"
+                  id="pswd"
+                  value={this.state.pswd}
+                  onChange={this.handleInputChange}
+                />
+            </InputGroup>
+
+            <Button type="submit" value="Log IN" color="primary" size="lg" block>Log in</Button>
+
+
+          </Form>
+
+
+
+          { this.printErrorMsg() }
         </div>
       </div>
+
     );
   }
 }
