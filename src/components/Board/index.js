2930734783d7f9147b706a6edac09768b416561d
R3nt0n <r3nt0n@protonmail.com>
2018-10-06 21:57:29 +0200

    Reactstrap

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { Button, InputGroup, InputGroupAddon, Input } from 'reactstrap';
+
 
 class Board extends Component {
   render() {
     return (
       <div className="App">
         <div className="container">
-          <input type="button" onClick={() => this.props.toggleLoginBoard()} value="Log OUT"/>
+          <InputGroup>
+            <InputGroupAddon addonType="prepend">$</InputGroupAddon>
+            <Input placeholder="Amount" type="number" step="1" />
+            <InputGroupAddon addonType="append">.00</InputGroupAddon>
+          </InputGroup>
+
+          <Button color="danger" type="button" onClick={() => this.props.toggleLoginBoard()} value="Log OUT">Log out</Button>
         </div>
       </div>
     );
